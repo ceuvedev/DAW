9b4816e2182db82f13ec97d9d661e0a1f574cee6
chan <chanvt@gmail.com>
2025-11-26 23:20:33 +0100

    proba

--- Programación/tasques/chanVoongTranOquesJoc.py
+++ Programación/tasques/chanVoongTranOquesJoc.py
@@ -3,12 +3,15 @@ import random
 def dados():
     dados = random.randrange(1, 6)
     print(f"Soy un dado de valor: {dados}")
+    return int(dados)
 print("Welcome to joc de la Oca")
 casillasTotales = int(input("Cuántas casillas quieres que tenga el juego? ")) 
-casillasTotales += 1 
 print(casillasTotales)
 posicionActual = 0
 
-#hay que hacer un bucle para que haga el algoritmo de las sumas en el rango de las casillas del juego
-for i in range (0, casillasTotales):
-    dados()
+#hay que hacer un bucle para que el juego no acabe hasta que se cumpla la condicion de que se llegue a las casillas totales. de momento la logica ha aplicar, es que se termine el bucle pasado las casillasTotales
+
+while posicionActual < casillasTotales:
+    posicionActual = posicionActual + dados()
+    print(f"Estoy en la posición {posicionActual}")
+        if posicionActual
