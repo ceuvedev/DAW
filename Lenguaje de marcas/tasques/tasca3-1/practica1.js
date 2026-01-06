91378c32212d761cee8f92425820c18331f7977c
Chan <chanvt@gmail.com>
2026-01-06 23:03:45 +0100

    lmi

---
+++ Lenguaje de marcas/tasques/tasca3-1/practica1.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// declarar variable tipo: nombre, cadena texto, booleano. 
+
+let numero = 9000;
+let texto = "Hola, esto forma parte de un ejercicio de LMI";
+let booleano = true;
+
+// mostrar por consola el valor de las variables
+
+console.log(numero);
+console.log(texto);
+console.log(booleano);  
+
+// mostrar el tipo de variable
+
+console.log(typeof numero);
+console.log(typeof texto);
+console.log(typeof booleano);
+
+// cambiar el valor de alguna variable e imprimir otra vez
+
+booleano = false;
+
+console.log(booleano);
+
