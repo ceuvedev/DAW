472ce4fdb2dbaace0a567a50012170f876f7081b
Chan <chanvt@gmail.com>
2026-02-25 13:00:39 +0100

    asdf

---
+++ IPO 1/tasques/noticia_modificacion_contrato.md
@@ -0,0 +1,68 @@
+### Chan Voong Tran
+
+
+## Chan Voong Tran
+
+### Unitat 14
+
+1. c
+2. c
+3. b
+4. a
+5. a
+6. b
+7. a
+8. a
+9. a
+10. a
+11. c
+12. a
+13. c
+14. b
+15. c
+16. c
+17. b
+
+
+#### Busqueu una notícia d’actualitat relacionada amb un supòsit d’extinció, modificació o suspensió d’un contracte de treball.
+
+
+[Noticia](https://www.brujulabike.com/despedido-publicar-strava-rutas-mtb-estando-baja/)
+
+#### Resumen
+
+Trabajador sufrió un accidente laboral el 4 de noviembre de 2024. La mutua conceció una baja de unos 20 días por un esguince de hombro con subluxación.
+
+El trabajador, dos días después, entre el 6 y el 23 de noviembre completa 7 rutinas de ejercicio con un alto nivel de exigencia física.
+
+El día 26 el trabajador acude al hospital por un dolor torácico y lo ingresan hasta el día 29. Al día siguiente vuelve a su rutina de ejercicio.
+
+Día 4 de diciembre le dan el alta por la lesión ya que puede reincorporarse a su trabajo.
+
+La empresa abre expediente disciplinario al considerar que las actividades que realizaba eran incompatibles con la recuperación y que demostraba capacidad para trabajar. El trabajador alega que en la mutua no le habían prohibido montar en bicicleta y que el motivo del despido era por su condición de representante de los trabajadores.
+
+El tribunal rechaza los argumentos, ya que aunque no toda actividad física es sancionable, la que interfiere con la curación sí lo es.
+
+Todo esto se resume a que se ha vulnerado la buena fe contractual y que justifica el despido disciplinario.
+
+
+
+
+#### Analitzar les conseqüències jurídiques del cas, indicant:
+*. Quina figura jurídica s’aplica (extinció, modificació o suspensió).
+    Extinció per despido disciplinario.
+*. Quin article o normativa laboral la regula.
+    Les causes de la extinció del contracte de treball están regualres pel Estatut dels treballadors.
+*. Quins drets o obligacions afecten a l’empresa i a la persona treballadora.
+    En aquesta situació on es un despido disciplinari. L'empresa té que notificar a la persona treballadora per escrit, indicant les motius, fecha d'efecte sin necessitat de preaviso.
+    No te perqué indemnitzar a la persona.
+    Per par del treballador por reclamar la decisió de l'empresa per concilició administrativa, si no isc bé, pot continuar el procés amb el judjat de lo social
+
+
+
+
+paraulas clau:
+
+a sentencia confirma íntegramente el fallo previo del Juzgado de lo Social nº1 de Oviedo 
+
+transgresión de la buena fe contractual”.
--- Programación/tasques/registreNotesChanVoongTran.java
+++ Programación/tasques/registreNotesChanVoongTran.java
@@ -14,10 +14,11 @@ class Alumne {
 public class registreNotesChanVoongTran {
 
     public static void main(String[] args) {
-        final int N = 3;
+        final int N = 100;
         Alumne alu[] = new Alumne[N];
         for (int i=0; i<N; i++) {
             alu[i] = new Alumne();
+            alu[i].nom = "";
         }
 
         int opcio;
@@ -33,32 +34,32 @@ public class registreNotesChanVoongTran {
             System.out.println("7. Esborrar tots els alumnes");
             System.out.println("8. Estadistiques");
             System.out.println("0. Sortir del programa");
-            System.out.print("Tria una opció: ");
+            System.out.println("Tria una opció: ");
 
-            opcio = funcionsIO.readIntC("Indica una opció");
+            opcio = funcionsIO.readIntC("Per favor, indica un num 1-8 o 0 per a ixir");
 
 
             switch (opcio) {
                 case 1:
-                    altaAlumne();
+                    altaAlumne(alu);
                     break;
                 case 2:
                     notaAlumne();
                     break;
                 case 3:
-                    buscarAlumne();
+                    buscarAlumne(alu);
                     break;
                 case 4:
-                    modificarAlumne();
+                    modificarAlumne(alu);
                     break;
                 case 5:
                     modificarNota();
                     break;
                 case 6:
-                    borrarAlumne();
+                    borrarAlumne(alu);
                     break;
                 case 7:
-                    wipe();
+                    wipe(alu);
                     break;
                 case 8:
                     estadistiques();
@@ -77,37 +78,90 @@ public class registreNotesChanVoongTran {
     }
 
     // Definin els metodos
-    static void altaAlumne (){
 
-
-
-
-
-
-    }
-    static void notaAlumne (){
-        System.out.println("Adios");
-    }
-    static void buscarAlumne (){
-        System.out.println("A quí busques? Indicam el nom: ");
-        for (int i = 0; i<N; i++) {
-
+    /*(1) Demanarà les dades personals d'un alumne (no les notes) i les guardarà.
+    Caldrà buscar una posició lliure del vector on posar l’alumne */
+    static void altaAlumne (Alumne[] alu){
+        for(int i=0; i < alu.length; i++ ) {
+            if (alu[i].nom.equals("")){
+                alu[i].nom = funcionsIO.readTextC("Dime el teu nom: ");
+                alu[i].tlf = funcionsIO.readIntC("Dime el teu telf: ");
+                alu[i].adre.carrer = funcionsIO.readTextC("Dime el teu carrer: ");
+                break;
+            }
         }
 
-        System.out.println("Adios");
     }
-    static void modificarAlumne (){
+
+    /* (2) Preguntarà quina avaluació i assignatura i anirà demanant per teclat les notes
+    corresponents de cada alumne donat d'alta (anirà mostrant el nom) i guardarà
+    eixes notes.*/
+    static void notaAlumne (){
+        //bucle niuat?
+        System.out.println("La teua nota: ");
+    }
+
+    /*(3) Preguntarà pel nom de l’alumne i mostrarà totes les seues dades. S’imagina
+    que els alumnes tenen tots un nom diferent.*/
+    static void buscarAlumne (Alumne[] alu){
+        System.out.println("A quí busques? Indicam el nom: ");
+        String buscarNom = funcionsIO.readTextC("Dime el nom a buscar: ");
+        for (int i = 0; i<alu.length; i++) {
+            if (alu[i].nom.equalsIgnoreCase(buscarNom)) {
+                System.out.println("Te encontre");
+                System.out.println("eres: " + alu[i].nom);
+                System.out.println("vives: " + alu[i].adre.carrer);
+                System.out.println("Tu telf es: " + alu[i].tlf);
+            } else {
+                System.out.println("No trobat :(");
+            }
+        }
+    }
+    /*(4) Pregunta el nom d'un alumne, mostrarà les seues dades, demanarà les noves
+    dades a modificar (nom, telèfon i adreça) i modificarà eixe alumne en la llista.*/
+
+    static void modificarAlumne (Alumne[] alu){
+        System.out.println("A quí vols modificar Indicam el nom: ");
+        String buscarNom = funcionsIO.readTextC("Dime el nom a buscar: ");
+        for (int i = 0; i<alu.length; i++) {
+            if (alu[i].nom.equalsIgnoreCase(buscarNom)) {
+                alu[i].nom = funcionsIO.readTextC("Com et diuen? ");
+                alu[i].adre.carrer = funcionsIO.readTextC("Quín carrer vius ara? ");
+                alu[i].tlf = funcionsIO.readIntC("Quín telf. tens ara? ");
+            }
+        }
         System.out.println("S'ha modificat l'alumne");
     }
+    /*(5) Pregunta el nom d'un alumne, mostrarà les seues dades, preguntarà quina
+    assignatura i quina avaluació, quina nota i la modificarà.*/
+
     static void modificarNota (){
         System.out.println("S'ha modificat la nota");
     }
-    static void borrarAlumne (){
-        System.out.println("Alumne esborrat");
+    /*(6) Pregunta el nom d'un alumne, mostrarà les seues dades, preguntarà si està
+    segur d'esborrar i, si és així, l'esborrarà de la llista.*/
+
+    static void borrarAlumne (Alumne[] alu){
+
+        System.out.println("A quí vols modificar Indicam el nom: ");
+        String buscarNom = funcionsIO.readTextC("Dime el nom a buscar: ");
+        for (int i = 0; i<alu.length; i++) {
+            if (alu[i].nom.equalsIgnoreCase(buscarNom)) {
+                alu[i].nom = "";
+            }
+        }System.out.println("Alumne esborrat");
     }
-    static void wipe (){
+    /* (7) Pregunta si està segur i, si és així, esborrarà tota la llista.*/
+
+    static void wipe (Alumne[] alu){
+
+        for (int i = 0; i<alu.length; i++) {
+            alu[i].nom = "";
+        }
         System.out.println("S'ha borrar totes les dades");
     }
+    /**/
+
     static void estadistiques(){
         System.out.println("Adios");
     }
